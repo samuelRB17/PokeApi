c2a78a0cf8a7295d674127c01aee127684da52e9
samuelRB17 <samuel.ramirezb@uniagustiniana.edu.co>
2025-10-06 19:54:55 +0000

    BotomFavoritos/AlmacenCapturado/aleatoriosCapturados

--- JS/Favoritos.js
+++ JS/Favoritos.js
@@ -1,3 +1,9 @@
-function Favoritos (){
-    document.getElementById("root").innerHTML= "Favoritos"
+function Favoritos(){
+    let favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
+    if(favoritos.length == 0){
+        document.getElementById("root").innerHTML = "no hay favoritos"
+    }else{
+        document.getElementById("root").innerHTML = GenerarLista(favoritos)
+    }
+    
 }
--- JS/Pokemon.js
+++ JS/Pokemon.js
@@ -1,7 +1,51 @@
+var esFavorito = false;
+
+// Función para agregar o quitar un Pokémon de favoritos
+function toggleFavorito(paramid, paramname) {
+
+    // Leer favoritos actuales desde localStorage
+    let favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
+    let existe = false
+
+    // Verificar si ya está guardado
+    for (let i = 0; i < favoritos.length; i++) {
+        if (favoritos[i].name === paramname) {
+            existe = true;
+            break;
+        }
+    }
+
+    if (existe == true) {
+        favoritos = favoritos.filter(poke => poke.name !== paramname);
+        esFavorito = false;
+    } else {
+        // Si no está, agregarlo
+        favoritos.push({
+            name: paramname,
+            url: `https://pokeapi.co/api/v2/pokemon/${paramid}/`
+        });
+        esFavorito = true;
+    }
+
+    // Guardar el array actualizado en localStorage
+    localStorage.setItem("favoritos", JSON.stringify(favoritos));
+
+    // Actualizar el icono en pantalla (si existe el botón)
+    const boton = document.querySelector(`#corazon-${paramid}`);
+    if (boton) boton.textContent = esFavorito ? "❤️" : "🤍";
+}
+
+
+
+
+
 async function Detalle (h){
     var root = document.getElementById("root");
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${h}`);
     const data = await res.json();
+    
+      favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
+    esFavorito = favoritos.some(poke => poke.name === data.name);
 
     // Construir tipos (siempre existen en la API)
     let tipos = data.types.map(t => t.type.name).join(", ");
@@ -23,6 +67,9 @@ async function Detalle (h){
             <p>Velocidad: ${data.stats[5].base_stat}</p>
             <p>Ataque: ${data.stats[1].base_stat} | Defensa: ${data.stats[2].base_stat}</p>
             <p>Ataque Especial: ${data.stats[3].base_stat} | Defensa Especial: ${data.stats[4].base_stat}</p>
+               <button onClick="toggleFavorito(${data.id}, '${data.name}')">
+        <span id="corazon-${data.id}">${esFavorito ? '❤️' : '🤍'}</span> Favorito
+      </button>
         </section>
     `;
 
